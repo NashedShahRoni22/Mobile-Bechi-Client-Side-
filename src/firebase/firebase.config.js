209bb238906303012760b27eeae780d79464f32d
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-11-23 21:59:12 +0600

    basic installation done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCn7PVCzuLvTWFEQk6v1YEUW9KKjPaZhKc",
+  authDomain: "mobile-bechi.firebaseapp.com",
+  projectId: "mobile-bechi",
+  storageBucket: "mobile-bechi.appspot.com",
+  messagingSenderId: "854817502465",
+  appId: "1:854817502465:web:16da14d61c33bed959d700"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
